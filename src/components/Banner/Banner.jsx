800b7dc3caadac27758d2482082db98ba078e547
mchowdhury2023 <chowdhury7135@saskpolytech.ca>
2023-10-18 15:49:20 -0600

    worked on to display extra section on my home page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Banner = () => {
+const Banner = ({ brands }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const next = (currentIndex + 1) % brands.length;
+    const id = setTimeout(() => setCurrentIndex(next), 3000);
+    return () => clearTimeout(id);
+  }, [currentIndex, brands.length]);
+
   return (
-    <div>
-        <h2 className='text-3xl text-center'>Banner</h2>
+    <div className="relative h-[500px]">
+      <div className="absolute w-full h-full bg-opacity-40 bg-black flex items-center justify-center">
+        <input
+          type="text"
+          placeholder="Search for brands..."
+          onChange={(e) => setSearch(e.target.value)}
+          className="p-2 rounded bg-white focus:outline-none focus:ring-2 focus:ring-blue-600"
+        />
+      </div>
+
+      {brands.map((brand, index) => (
+        <img
+          key={brand._id}
+          src={brand.photo}
+          alt={brand._id}
+          className={`absolute w-full h-full object-cover transition-opacity duration-1000 ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import Banner from '../../components/Banner/Banner'
-import { useLoaderData } from 'react-router-dom'
-import Productcard from '../../components/ProductCard/Productcard';
-import BrandCard from '../../components/BrandCard/BrandCard';
+import React, { useEffect, useState } from "react";
+import Banner from "../../components/Banner/Banner";
+import BrandCard from "../../components/BrandCard/BrandCard";
+import { Carousel } from "react-bootstrap";
 
 const Home = () => {
+  const [brands, setBrands] = useState([]);
+  const [featuredProducts, setFeaturedProducts] = useState([]);
 
-    //const loadedProducts = useLoaderData();
-    const [brands, setBrands] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/brands")
+      .then((res) => res.json())
+      .then((data) => setBrands(data));
+  }, []);
 
-    useEffect(() => {
-        fetch("http://localhost:5000/brands")
-            .then(res => res.json())
-            .then(data => setBrands(data));
-    }, []);
-
-
-    //const [products, setProducts] = useState(loadedProducts);
+  useEffect(() => {
+    fetch("http://localhost:5000/products?limit=5")
+      .then((res) => res.json())
+      .then((data) => setFeaturedProducts(data));
+  }, []);
 
   return (
     <div>
-    <Banner></Banner>
-    <div>
-        <h2 className="text-2xl bg-slate-200 font-semibold rounded-md mb-4 text-center">
-            Brands:
-        </h2>
-    </div>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {
-            brands.map((brand) => (
-                <BrandCard key={brand._id} brandName={brand._id} photo={brand.photo}></BrandCard>
-            ))
-        }
-    </div>
-</div>
-  )
-}
+      <Banner brands={brands}></Banner>
 
-export default Home
+      <div className="mt-10">
+        <h2 className="text-2xl bg-slate-200 font-semibold rounded-md mb-4 text-center">
+          Brands:
+        </h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {brands.map((brand) => (
+          <BrandCard
+            key={brand._id}
+            brandName={brand._id}
+            photo={brand.photo}
+          ></BrandCard>
+        ))}
+      </div>
+      <div>
+        <h2 className="text-5xl text-center mt-10">Our Top Picks For You</h2>
+      </div>
+
+      <Carousel className="mt-8" pause={false}>
+        {featuredProducts.map((product, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-3/4 mx-auto h-96 object-cover mb-10"
+              src={product.photo}
+              alt={product.modelName}
+            />
+            <Carousel.Caption className="bg-black bg-opacity-50 p-3 rounded mb-10">
+              <h3 className="text-white">{product.brandName}</h3>
+              <p className="text-white">{product.modelName}</p>
+              <p className="text-white">{product.year}</p>
+              <p className="text-white">${product.price}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Home;
